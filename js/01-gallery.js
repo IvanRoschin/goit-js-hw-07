e6b5bd52942c790ddc76f51cf326391d4e6ab00d
Ivan Roschin <ivan.roschin86@gmail.com>
2022-08-18 23:30:27 +0300

    update-gallery
    Add modal-close function js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -34,4 +34,49 @@ console.log(galleryCountainer);
 
 //4. Открытие модального окна по клику на элементе галереи.
 
+/*
+ * 1. Открыть и закрыть по кнопке
+ * 2. Закрыть по клику в бекдроп: onBackDropClick
+ * 3. Закрыть по ESC: onEscapeKeypress
+ *
+ * В CSS есть класс show-modal, который необходимо добавить на body при открытии модалки
+ */
+
+const refs = {
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector(".js-backdrop"),
+};
+
+refs.openModalBtn.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("show-modal");
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("show-modal");
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    console.log("Клик по бекдроп");
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  console.log("event.code", event.code);
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE; //равенство в переменную
+
+  if (event.code === ESC_KEY_CODE) {
+    onCloseModal();
+  }
+}
+
 //5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки
